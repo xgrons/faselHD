ba246d86a8d9be8742a3d9e41041ddd205253a00
Nadji-abidi <53299787+Nadji-abidi@users.noreply.github.com>
2021-10-14 13:01:33 +0100

    Add files via upload

--- faselHD.py
+++ faselHD.py
@@ -3,48 +3,82 @@ import requests
 import os
 import re
 from bs4 import BeautifulSoup as bs
+from selenium.webdriver import Chrome
+from youtube_dl.utils import random_user_agent
+soup = lambda url : bs(requests.get(url).content,"html.parser")
 
 def search(user_input):
     search_URL = "https://www.faselhd.pro/?s=" + user_input.strip()
-    search_result = bs(requests.get(search_URL).content, "html.parser")
+    search_result = soup(search_URL)
     results = search_result.select(".postDiv")
     tit_url = [(i.select_one(".h1").text, i.a["href"]) for i in results]
-    return {k: v for k, v in tit_url}
+    return dict(tit_url)
 
 def display_results(lists):
     choises = list(lists.keys())
     for i in range(len(choises)):
-        print(f"{i}. \x1b[33m{choises[i]}\x1b[0m")
+        print(f"{i + 1}. \x1b[33m{choises[i]}\x1b[0m")
     choise = int(input("\n\x1b[41m\x1b[37mChoose one \x1b[0m : "))
-    return choises[choise]
+    return choises[choise - 1]
+
+def seasons(seasonList):
+    seasons_num = dict([(i.select_one(".title").text,i.div["data-href"]) for i in seasonList])
+    choise = display_results(seasons_num)
+    return "https://www.faselhd.pro/?p=" + seasons_num[choise]
 
 def select_episodes(media_url):
-    media_htm = bs(requests.get(media_url).text,'html.parser')
+    media_htm = soup(media_url)
+    seasons_list = media_htm.select("#seasonList > div")
+    if seasons_list : media_htm = soup(seasons(seasons_list))
     episodes = [ i["href"] for i in media_htm.select(".epAll a") ]
-    pattern = r"[%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9]-(\d+)"
-    episodes_num = [int("".join(re.findall(pattern,i))) for i in episodes]
-    start,end = [ input(f"\n {i} EP : ") for i in ["Start on","End in"] ]
-    sel = lambda i,opt : opt if i == "" else episodes_num.index(int(i))
-    return episodes[sel(start,0):sel(end,-2) + 1]
+    if not(episodes) : return [media_url]
+    else :
+        pattern = r"[%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9]-(\d+)"
+        try : episodes_num = [int("".join(re.findall(pattern,i))) for i in episodes]
+        except ValueError : episodes_num = list(range(1,len(episodes) + 1))
+        print(f"Episodes : {episodes_num[0]}-{episodes_num[-1]}")
+        start,end = [ input(f"\n {i} EP : ") for i in ["Start on","End in"] ]
+        sel = lambda i,opt : opt if i == "" else episodes_num.index(int(i))
+        return episodes[sel(start,0):sel(end,len(episodes))]
 
-def download(links,series=True):
+def opn_brow(site) :
+    driver = Chrome("/home/i5/Scripts/FaselHD/chromedriver")
+    driver.get(site)
+    html = driver.page_source
+    driver.close()
+    return html
+
+def CQuality(available,prefer) :
+    all_qual = ["360","480","720","1080"]
+    return prefer if prefer in available else CQuality(available,all_qual[all_qual.index(prefer) - 1])
+
+def download(links,folder,normal=True):
+    if not(normal) : quality = input("Choose Your Preferde Resolution : [1080,720,480,360] ? ")
     for link in links:
-        link = bs(requests.get(link).text,"html.parser")
-        downloadLink = link.select_one(".downloadLinks").a["href"]
-        down_page = bs(requests.get(downloadLink).text,'html.parser')
-        title = down_page.find("p",{"class":"info-item"}).span.text
-        url = down_page.select_one(".dl-link").a["href"]
+        link = soup(link)
+        title = link.select_one(".h3").text
+        title = " ".join(re.findall(r"\d+|\w+",title)) + ".mp4"
         print(f"\n\x1b[41m\x1b[37mDownloading \x1b[0m ===> {title}")
-        os.system(f"wget '{url}' -O '{title}' --tries 10 -c --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'")
+        if normal :
+            downloadLink = link.select_one(".downloadLinks").a["href"]
+            down_page = soup(downloadLink)
+            url = down_page.select_one(".dl-link").a["href"]
+            os.system(f"wget '{url}' -O '{title}' --tries 10 -c --user-agent='{random_user_agent()}'")
+        else : 
+            link = opn_brow(link.find("iframe")["src"])
+            videoURL = re.findall("\"file\":\"(.*)\",\"hlshtml\"",link)[0].replace("\\","")
+            available = re.findall(r",*(\d+)+,p*",videoURL)
+            if available : newVideoURL = re.sub(r",(.*),",f",{CQuality(available,quality)}",videoURL)
+            os.system(f"downloadm3u8 -o '{folder}/{title}' '{newVideoURL or videoURL}'")
 
 def main() :
     main_page = search(input("Search : "))
+    while main_page == {} : 
+        print("\n\n----- \x1b[41mNo Results\x1b[0m ----\n\n")
+        main_page = search(input("Search : "))
     selected = display_results(main_page)
-    try : 
-        os.mkdir(selected)
-        os.chdir(selected)
-    except FileExistsError : os.chdir(selected)
     episodes = select_episodes(main_page[selected])
-    download(episodes)
-
-main()
+    download(episodes,"/home/i5/Scripts/Downloads/"+selected,normal=False)
+    
+try : main()
+except KeyboardInterrupt : print("\n\nExit .....")
